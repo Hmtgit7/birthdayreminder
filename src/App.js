9d5cc187f6f42e213241254fbc8d895f34507def
Hmtgit7 <hmtlohar3579@gmail.com>
2023-02-02 00:57:31 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+console.log(`<h1>hello world <h1>`)
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome.......</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
